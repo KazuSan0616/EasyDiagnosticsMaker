4e6a00d40cd43a98df4b86d256bd911008810e0a
yamamura <yamamura@aimek.co.jp>
2020-03-06 17:57:41 +0900

    DBアクセスクラスを追加、result画面で簡易テストコードを追加(DB追加できることを確認)

--- diagnostics_maker/control/db_accessor.py
+++ diagnostics_maker/control/db_accessor.py
@@ -1,17 +1,23 @@
-from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy import create_engine
+from sqlalchemy import Column, String, Integer
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.orm.exc import NoResultFound
 
-engine = create_engine('sqlite://sample_db.sqlite3', echo=True)
-
 Base = declarative_base()
 
+
+class DbAccessor:
+    def __init__(self):
+        self.engine = create_engine("sqlite:///sample_db.sqlite3", echo=True)
+        Session = sessionmaker(bind=self.engine)
+        self.session = Session()
+        Base.metadata.create_all(self.engine)
+
 class Diagnostic(Base):
-    __tablename__ = 'diagnostics'
+    __tablename__ = "diagnostics"
 
     id = Column(Integer, primary_key=True)
-    title = Column(String)
-    text = Column(String)
+    title = Column(String(10))
+    baseText = Column(String(10))
 
-Base.metadata.create_all(bind=engine)
--- diagnostics_maker/views/result.py
+++ diagnostics_maker/views/result.py
@@ -1,8 +1,20 @@
 from flask import Blueprint
+from diagnostics_maker.control.db_accessor import DbAccessor, Diagnostic
 
 mod = Blueprint("result", __name__, url_prefix="/result")
 
 @mod.route("/")
 def index():
+
+    db = DbAccessor()
+    diag = Diagnostic()
+    #diag.id = 1
+    diag.title = "hoge"
+    diag.baseText = "hogehoge"
+
+    db.session.add(diag)
+    db.session.commit()
+
+
     return "Hello. This page is result."
 
