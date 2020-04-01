a96c11357138f03138e13ad6791d2a7ddd5cb90f
ymdevx3 <y.mori.707@gmail.com>
2020-04-02 02:50:34 +0900

    診断作成画面＆ホームからの遷移処理の追加

--- diagnostics_maker/views/home.py
+++ diagnostics_maker/views/home.py
@@ -1,8 +1,18 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, Flask, request, redirect,
+    url_for, render_template,
+)
+from .make import MakeForm
+from ..control.db_accessor import DbAccessor, Diagnostic
 
 mod = Blueprint("home", __name__, url_prefix="/")
 
 @mod.route("/")
 def index():
-    return "Hello. This page is home."
+    #all_diag = Diagnostic.query.all()
+    return render_template("home.html")
 
+@mod.route("/make", methods=["GET", "POST"])
+def show_make():
+    form = MakeForm(request.form)
+    return render_template("make.html", form=form)
--- diagnostics_maker/views/make.py
+++ diagnostics_maker/views/make.py
@@ -1,7 +1,44 @@
-from flask import Blueprint
+from flask import (
+    Blueprint, Flask, request, redirect,
+    url_for, render_template,
+)
+from wtforms import (
+    Form, StringField, TextField, TextAreaField, validators 
+)
+from ..control.db_accessor import DbAccessor, Diagnostic
 
 mod = Blueprint("make", __name__, url_prefix="/make")
 
-@mod.route("/")
+@mod.route("/", methods=["GET", "POST"])
 def index():
-    return "Hello. This page is make."
+    return render_template("home.html")
+
+@mod.route("/create", methods=["GET", "POST"])
+def make_diag():
+    form = MakeForm(request.form)
+
+    # if form.validate_on_submit():
+    if form.validate():
+        # db = DbAccessor()
+        # diag = Diagnostic()
+        # form.populate_obj(diag)
+        
+        # diag.title = "hoge"
+        # diag.baseText = "hogehoge"
+
+        # db.session.add(diag)
+        # db.session.commit()
+
+        return render_template("home.html")
+
+    return render_template("make.html", form=form)
+
+class MakeForm(Form) :
+    title = TextField("診断タイトル*", validators=[
+        validators.Required(message="診断タイトルを入力してください")
+    ])
+
+    baseText = TextAreaField("診断結果基本テキスト*", validators=[
+        validators.Required(message="診断結果基本テキストを入力してください")
+    ])
+
