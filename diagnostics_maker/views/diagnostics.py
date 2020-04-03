e1dd353edb9b257f49c444f94c838f24ad423a85
yamamura <yamamura@aimek.co.jp>
2020-04-03 15:21:37 +0900

    診断画面、結果画面を暫定対応

--- diagnostics_maker/views/diagnostics.py
+++ diagnostics_maker/views/diagnostics.py
@@ -1,8 +1,22 @@
-from flask import Blueprint
-from flask import render_template
+from flask import (
+    Blueprint, render_template, request)
+from wtforms import (
+    Form, StringField, TextField, TextAreaField, validators 
+)
+from diagnostics_maker.control.db_accessor import DbAccessor, Diagnostic
 
 mod = Blueprint("diagnostics", __name__, url_prefix="/diagnostics")
 
-@mod.route("/")
-def index():
-    return render_template("diagnostics.html")
+@mod.route("/<id>", methods=["GET", "POST"])
+def index(id = None):
+    db = DbAccessor()
+    diag = db.session.query(Diagnostic).get(id)
+
+    form = MakeDiagnosticsForm(request.form)
+    return render_template("diagnostics.html", form=form, diag=diag, id=id)
+
+
+class MakeDiagnosticsForm(Form) :
+    name = TextField("username", validators=[
+        validators.Required(message="名前を入力してください")
+    ])
--- diagnostics_maker/views/result.py
+++ diagnostics_maker/views/result.py
@@ -1,20 +1,55 @@
 from flask import Blueprint
-from diagnostics_maker.control.db_accessor import DbAccessor, Diagnostic
+from flask import (
+    Blueprint, render_template, request)
+from diagnostics_maker.control.db_accessor import DbAccessor, Diagnostic, DiagnosticLists
+import datetime
 
 mod = Blueprint("result", __name__, url_prefix="/result")
 
-@mod.route("/")
-def index():
+@mod.route("/<id>", methods=["POST"])
+def index(id = None):
+
+    name = request.form["name"]
 
     db = DbAccessor()
-    diag = Diagnostic()
+    diag = db.session.query(Diagnostic).get(id)
+
+    itemCount = db.session.query(DiagnosticLists).count()
+    itemList = db.session.query(DiagnosticLists).filter(DiagnosticLists.diagId == id)
     #diag.id = 1
-    diag.title = "hoge"
-    diag.baseText = "hogehoge"
+    #diag.title = "hoge"
+    #diag.baseText = "hogehoge"
 
-    db.session.add(diag)
-    db.session.commit()
+    #db.session.add(diag)
 
+    #list1 = DiagnosticLists()
+    #list1.id = diag.id
+    #list1.listItem = "item1"
+    #db.session.add(list1)
 
-    return "Hello. This page is result."
+    #list2 = DiagnosticLists()
+    #list2.id = diag.id
+    #list2.listItem = "item2"
+    #db.session.add(list2)
 
+    #db.session.commit()
+    
+    text = generateDiagResult(diag.baseText, name, itemList)
+
+    return render_template("result.html", title=diag.title, baseText=text)
+
+#名前と日付から本日の値を取得する
+def createTodayValueFromName(name):
+    now = datetime.datetime.now()
+    todayValue = now.year * 10000 + now.month * 100 + now.day
+    nameValue = 0
+    for char in name:
+        nameValue += ord(char)
+    return todayValue + nameValue
+
+#表示テキスト作成処理
+def generateDiagResult(baseText, name, itemList):
+    lIdx = createTodayValueFromName(name) % itemList.count()
+    genText = baseText.replace("[USER]", name).replace("[LIST]", itemList[lIdx].listItem)
+    
+    return genText
