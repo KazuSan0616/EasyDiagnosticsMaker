97e3dafb4cd8235aae7ed4c7958a14ba3f036064
yamamura <yamamura@aimek.co.jp>
2020-04-03 17:19:49 +0900

    不要な処理、データを削除

--- diagnostics_maker/views/result.py
+++ diagnostics_maker/views/result.py
@@ -16,23 +16,6 @@ def index(id = None):
 
     itemCount = db.session.query(DiagnosticsList).count()
     itemList = db.session.query(DiagnosticsList).filter(DiagnosticsList.diagId == id)
-    #diag.id = 1
-    #diag.title = "hoge"
-    #diag.baseText = "hogehoge"
-
-    #db.session.add(diag)
-
-    #list1 = DiagnosticLists()
-    #list1.id = diag.id
-    #list1.listItem = "item1"
-    #db.session.add(list1)
-
-    #list2 = DiagnosticLists()
-    #list2.id = diag.id
-    #list2.listItem = "item2"
-    #db.session.add(list2)
-
-    #db.session.commit()
     
     text = generateDiagResult(diag.baseText, name, itemList)
 
